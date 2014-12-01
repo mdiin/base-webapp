ad53583c7bed41f8b97ba0b7a9159adf42a8ba44
Matthias Diehn Ingesman <matthias@ingesman.dk>
2014-12-01 21:10:58 +0100

    Isolate state mutation in events namespace.

--- src/cljs/personal_photos_reagent/events.cljs
+++ src/cljs/personal_photos_reagent/events.cljs
@@ -17,74 +17,107 @@
 
 ;; # Picture events
 (defonce select-picture-chan (async/chan))
+
 (async/sub event-publisher :select-picture select-picture-chan)
+
+(defn- toggle-select-picture
+  [pictures-state picture-id album-name]
+  (let [existing-selections (get-in pictures-state [picture-id :selected])]
+    (if (contains? existing-selections album-name)
+      (update-in pictures-state [picture-id :selected] disj album-name)
+      (update-in pictures-state [picture-id :selected] conj album-name))))
+
+(defn- select-picture-event
+  [event]
+  (let [current-pictures-state @(app-state :pictures)
+        picture-id-to-select (get-in event [:payload :picture :id])
+        album-to-select-picture-in (get-in event [:payload :album])
+        new-pictures-state (toggle-select-picture current-pictures-state picture-id-to-select album-to-select-picture-in)]
+    (compare-and-set! (app-state :pictures) current-pictures-state new-pictures-state)))
+
 (defn- process-select-picture-events []
   (go
     (while true
-      (let [event (async/<! select-picture-chan)
-            pic-id (get-in event [:payload :picture :id])
-            album-name (get-in event [:payload :album])
-            existing-selections (get-in @(app-state :pictures) [pic-id :selected])]
-        (if (contains? existing-selections album-name)
-          (swap! (app-state :pictures) update-in [pic-id :selected] disj album-name)
-          (swap! (app-state :pictures) update-in [pic-id :selected] conj album-name))))))
+      (let [event (async/<! select-picture-chan)]
+        (select-picture-event event)))))
+
 (process-select-picture-events)
+
+;; ## Deselect pictures events
 
 (defonce deselect-pictures-chan (async/chan))
 (async/sub event-publisher :deselect-pictures deselect-pictures-chan)
+
+(defn- deselect-picture
+  [[pic-id pic]]
+  [pic-id (assoc pic :selected #{})])
+
+(defn- deselect-pictures
+  [ps]
+  (into {}
+        (map deselect-picture ps)))
+
 (defn- deselect-pictures-event
-  [e]
-  (letfn [(deselect-picture
-            [[pic-id pic]]
-            [pic-id (assoc pic :selected #{})])
-          (deselect-pictures
-            [ps]
-            (into {}
-                  (map deselect-picture ps)))]
-    (swap! (app-state :pictures) deselect-pictures)))
+  [event]
+  (let [current-pictures-state @(app-state :pictures)
+        new-pictures-state (deselect-pictures current-pictures-state)]
+    (compare-and-set! (app-state :pictures) current-pictures-state new-pictures-state)))
 
 (defn- process-deselect-pictures-events []
   (go
     (while true
       (let [event (async/<! deselect-pictures-chan)]
         (deselect-pictures-event event)))))
+
 (process-deselect-pictures-events)
 
+;; ## Pictures changed events
 (defonce pictures-changed-chan (async/chan))
 (async/sub event-publisher :pictures-changed pictures-changed-chan)
+
+(defn- pictures-changed-event
+  [event]
+  (let [current-picture-state @(app-state :pictures)
+        current-album-state @(app-state :albums)
+        pictures (vals current-picture-state)
+        unsorted-pictures-ids {"No album" (map :id (remove (comp seq :albums) pictures))}
+        album-pictures-ids (apply merge-with concat (mapcat #(for [a (:albums %)] 
+                                                               {a [(:id %)]})
+                                                            pictures))
+        new-album-state (merge album-pictures-ids unsorted-pictures-ids)]
+    (compare-and-set! (app-state :albums) current-album-state new-album-state)))
+
 (defn- process-pictures-changed-events []
   (go
     (while true
-      (let [event (async/<! pictures-changed-chan)
-            pictures (get event :payload)
-            unsorted-pictures {"No album"
-                               (map :id 
-                                    (remove (comp seq :albums)
-                                            pictures))}
-            pictures-albums (apply merge-with concat (mapcat #(for [a (:albums %)] 
-                                                                {a [(:id %)]})
-                                                             pictures))
-            albums (merge pictures-albums unsorted-pictures)]
-        (reset! (app-state :albums) albums)))))
+      (let [event (async/<! pictures-changed-chan)]
+        (pictures-changed-event event)))))
+
 (process-pictures-changed-events)
 
 ;; ## Select album events
 (defonce select-album-chan (async/chan))
 (async/sub event-publisher :select-album select-album-chan)
 
+(defn- add-album-to-picture
+  [album picture]
+  (update-in picture [:albums] conj album))
+
 (defn- add-pictures-to-album
-  [pictures album]
-  ())
+  [pictures-map album]
+  (let [pictures (vals pictures-map)
+        selected-pictures (filter (comp seq :selected) pictures)
+        with-album (map (partial add-album-to-picture album) selected-pictures)
+        with-album-map (into {} (map #(vector (:id %) %) with-album))]
+    (merge pictures-map with-album-map)))
 
 (defn- select-album-event
-  [e]
-  (let [album (get e :payload)
-        pictures @(app-state :pictures)
-        ps (vals pictures)
-        selected-pictures (filter (comp seq :selected) ps)]
-    (doseq [p selected-pictures]
-      (swap! (app-state :pictures) update-in [(:id p) :albums] conj album))
-    (publish-event :event :pictures-changed :payload (vals @(app-state :pictures)))))
+  [event]
+  (let [album (get event :payload)
+        current-picture-state @(app-state :pictures)
+        with-new-albums (add-pictures-to-album current-picture-state album)]
+    (when (compare-and-set! (app-state :pictures) current-picture-state with-new-albums)
+      (publish-event :event :pictures-changed))))
 
 (defn- process-select-album-events []
   (go
